395504111aab4c193fe61604e22bb0587277b161
alexzhaohong <alexhong96@gmail.com>
2020-08-23 16:54:48 -0700

    Update basicChaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -11,10 +11,22 @@
 var fs = require('fs');
 var Promise = require('bluebird');
 
+var nodeStyle = require('./callbackReview.js');
+Promise.promisifyAll(nodeStyle);
+Promise.promisifyAll(fs);
 
+const myFunctions = require('./promisification.js');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return nodeStyle.pluckFirstLineFromFileAsync(readFilePath)
+    .then((firstLine) => firstLine) //(1)
+    .then((gitHubHandle) => {
+      return myFunctions.getGitHubProfileAsync(gitHubHandle);
+    }) //(2)
+    .then((body) => {
+      stringbody = JSON.stringify(body);
+      return fs.writeFileAsync(writeFilePath, stringbody, 'utf8');
+    }); //(3)
 };
 
 // Export these functions so we can test them
